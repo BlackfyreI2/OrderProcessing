069d44c581a58ec33dadf104cda48e705896a377
BlackfyreI2 <pisit.srib@ku.th>
2026-08-28 16:29:03 +0700

    Update GiftWrapDecorator.java

--- src/GiftWrapDecorator.java
+++ src/GiftWrapDecorator.java
@@ -1,4 +1,3 @@
-/** Decorator (Part 4): ห่อของขวัญ +50 บาท */
 public class GiftWrapDecorator extends ShipmentDecorator {
     private static final double GIFT_WRAP_FEE = 50.0;
 
@@ -6,15 +5,21 @@ public class GiftWrapDecorator extends ShipmentDecorator {
         super(wrappedShipment);
     }
 
-    @Override public String getInfo() {
-        // TODO (4a): คืน info ของตัวที่ห่อ แล้วต่อท้ายด้วย " + Gift Wrapped"
-        //   hint: wrappedShipment.getInfo() + " + Gift Wrapped"
-        return /* ====== replace this ====== */ wrappedShipment.getInfo();
+    @Override 
+    public String getInfo() {
+    
+        
+        /* ====== fill in here ====== */
+        return wrappedShipment.getInfo() + " + Gift Wrapped";
+        /* ========================== */
     }
 
-    @Override public double getCost() {
-        // TODO (4b): คืน cost ของตัวที่ห่อ บวกด้วย GIFT_WRAP_FEE
-        //   hint: wrappedShipment.getCost() + GIFT_WRAP_FEE
-        return /* ====== replace this ====== */ wrappedShipment.getCost();
+    @Override 
+    public double getCost() {
+      
+        
+        /* ====== fill in here ====== */
+        return wrappedShipment.getCost() + GIFT_WRAP_FEE;
+        /* ========================== */
     }
 }
